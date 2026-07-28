e259578bad1102e293d1bd9c7d36943766edebc7
hadyshfayy126-alt <hadyshfayy126@gmail.com>
2026-07-28 16:56:14 +0000

    Copy files before install

--- install.sh
+++ install.sh
@@ -16,6 +16,10 @@ git clone https://github.com/hadyshfayy126-alt/Prime-Core.git
 
 cd Prime-Core
 
+mkdir -p /usr/local/prime-core
+cp -r ./* /usr/local/prime-core/
+cd /usr/local/prime-core
+
 chmod +x x-ui.sh
 
 bash x-ui.sh install
